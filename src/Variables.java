3598de1006af7641dd2e4ae29e9d0a0c5f14f697
Alejandra <alejandrabp9@gmail.com>
2018-07-01 21:29:08 +0200

    Inicio de Repositorio

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int edad = 35 ; //Declaración e iniciación variable en una única linea
+		
+		/*la siguiente instruccion imprime en consola(System.out)el valor de la variable 
+		 edad
+		 */
+		
+		System.out.println(edad);
+		edad=75;
+		
+		System.out.println(edad);
+	}
+
+}
